ed9a20bbe482e0ddcef1e6939b6adab7b19d55e5
ahnaimi <naimi.amirhosein.gt@gmail.com>
2022-12-09 02:42:55 -0600

    add HELLOAPIVIEW AND THIS URL PATH (7)

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth import authenticate
 
 
 class UserModelManager(BaseUserManager):
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+
+    def get(self, request):
+        first = [
+            "hello"
+            "bye"
+        ]
+        return Response({"first": first, "second": "this is just a sentence"})
