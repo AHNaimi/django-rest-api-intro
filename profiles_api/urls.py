448305fa62ab7df058558aa8fafe074d9a70f074
ahnaimi <naimi.amirhosein.gt@gmail.com>
2022-12-09 05:40:46 -0600

    add and api that work with viewset instead of APIVIEW (9)

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('hello_viewset',views.HelloViewSet, basename='hello_viewset')
 
 
 app_name = 'api'
 urlpatterns = [
-    path('hello/', views.HelloApiView.as_view(), name='hello')
+    path('hello/', views.HelloApiView.as_view(), name='hello'),
+    path('', include(router.urls))
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from . import serializers
 from rest_framework import status
+from rest_framework import viewsets
 
 
 class HelloApiView(APIView):
@@ -33,4 +34,43 @@ class HelloApiView(APIView):
         return Response({"methode": "dispatch"})
 
     def delete(self, request, pk=None):
+        return Response({"methode": "delete"})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """test API ViewSet"""
+    class_srz = serializers.HelloSrz
+
+    def list(self, request):
+        """return a hello msg and its methode is get"""
+        x = [
+            'viewsets not use for complex functions',
+            'for simple api you can use viewset '
+        ]
+
+        return Response({"msg": "some msg", "viewset": x})
+
+    def create(self, request):
+        """this function roles POST methode function"""
+        srz = self.class_srz(data=request.data)
+        if srz.is_valid():
+            name = srz.validated_data.get('name')
+            msg = f'hello {name}'
+            return Response({"msg": msg})
+        else:
+            return Response(
+                srz.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, requests, pk=None):
+        return Response({"methode": "get"})
+
+    def update(self, request, pk=None):
+        return Response({"methode": "put"})
+
+    def partial_update(self, request, pk=None):
+        return Response({"methode": "patch"})
+
+    def destroy(self, request, pk=None):
         return Response({"methode": "delete"})
